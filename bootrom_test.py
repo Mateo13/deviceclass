b6c00a8311a90056149a7ada8a2a89cb548fe117
ngrauel <ngrauel@extremenetworks.com>
2015-07-07 14:27:35 -0400

    Updates to make this class inherit from 'test' class

--- bootrom_test.py
+++ bootrom_test.py
@@ -1,24 +1,32 @@
 #This test will execute 'mt' from BootROM to test all memory.
 
 #Imports
+from test import Test
 from device import Device
 
 #Define class to execute test.
-class BootROMTest(object):
+class BootROMTest(Test):
 	#Initialize class.
-	def __init__(self, telnet_device):
+	def __init__(self, name, telnet_device):
+		super().__init__(name)
 		self.tn = telnet_device
 		
 	#Check if test passed.
-	def check(self):
+	def checkResult(self):
+		print("////////////////////////////////////////////////////////////")
+		print("mtest execution completed")
 		if b'0 errors' in self.res:
-			print("Test Result: PASS\n")
+			print("Test Result: PASS")
 		else: 
-			print("Test Result: FAIL\n")
+			print("Test Result: FAIL")
+		print("////////////////////////////////////////////////////////////")
 
 	#Execute test.
 	def execute(self):
 		self.tn.login()
+		print("===============================================================================")
+		print("Beginning " + self.name)
+		print("===============================================================================")
 		print("Reseting device for BootROM testing...")
 		self.tn.reset()
 		self.tn.read_until('enter the bootrom:')
@@ -29,12 +37,10 @@ class BootROMTest(object):
 		self.res = self.tn.read()
 		self.res = self.tn.read_until('BootRom >')
 		print(self.res)
-		self.tn.write('boot\n')
-		self.check()
-		
+		self.tn.write('boot\n')		
 	
 if __name__ == '__main__':
 	tel = Device('EXOS', '10.52.2.33', 2009)
-	test = BootROMTest(tel)
+	test = BootROMTest('BootROM Test', tel)
 	test.execute()
-	
+	test.checkResult()
