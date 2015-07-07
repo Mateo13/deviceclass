c31b4e1e71e26af3ca48b89b97a50e8b7b1fad5c
ngrauel <ngrauel@extremenetworks.com>
2015-07-07 14:26:03 -0400

    Main file for test suite execution

---
+++ executeTests.py
@@ -0,0 +1,35 @@
+#Main class to run all test execution.
+
+#Imports
+from test import Test
+from device import Device
+import install_test
+import create_delete_VLANS
+import rescue_test
+import bootrom_test
+
+dut = Device('EXOS', '10.52.2.33', 2009)
+tests = []
+
+#Add InstallTest to list of tests to execute.
+t = install_test.InstallTest('Install Test', dut, '10.52.4.40', 'firmware/images/summitX-16.1.1.4.xos')
+t.numIter = 2
+tests.append(t)
+
+#Add VLAN test to list of tests to execute and update num_ports.
+t = create_delete_VLANS.VLANTest('VLAN Test', dut)
+t.num_ports = 48
+t.numIter = 2
+tests.append(t)
+
+#Add BootROM test to list of tests to execute.
+t = bootrom_test.BootROMTest('BootROM Test', dut)
+tests.append(t)
+
+#Add Rescue Image Test to list of tests to execute.
+t = rescue_test.RescueTest('Rescue Image Test', dut, '10.52.4.40', 'firmware/images/summitX-16.1.1.4.xos')
+tests.append(t)
+
+for i in tests:
+	i.execute()
+	i.checkResult()
