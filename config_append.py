dc7f8131686f041454fba294f1f0fb8495a251b1
Matt <mateo13@gmail.com>
2015-07-07 08:53:59 -0400

    added config_append.py, a test to clear/apply config.

---
+++ config_append.py
@@ -0,0 +1,35 @@
+
+from device import Device
+
+
+
+
+
+if __name__ == '__main__':
+	f = open('log.txt', 'w')
+	DUT = Device('EOS', '10.52.2.222', 10022)
+	DUT.write('\n')
+	DUT.login()
+	f.write(DUT.read_until_prompt())
+	
+	for x in range(500):
+		print('*** Starting iteration %d ***' % x)
+		f.write('*** Starting iteration %d ***\n\n' % x)
+		DUT.clearConfig()
+		# It takes about 3 minutes to come back online.
+		time.sleep(3*60)
+		DUT.login()
+		output = DUT.read_until_prompt()
+		f.write(output)
+		# append the backup config.
+		DUT.write(['config slot\\backup2.cfg append','y','y'])
+		# This should wait for the append to finish.
+		output = DUT.read_until_prompt()
+		f.write(output)
+		if 'MII' in output:
+			print('Found an issue')
+			break
+		else:
+			print('No issue found')
+		#DUT.clearConfig()
+	f.close()
