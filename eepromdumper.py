eb4409d26d75f91659adff617ff01a3e8c9580b9
Matt <mateo13@gmail.com>
2016-01-05 13:13:14 -0500

    Changed how good file is parsed and added check for number of eeproms found.

--- eepromdumper.py
+++ eepromdumper.py
@@ -2,6 +2,8 @@ import D
 import time
 
 geteeprom = 'debug hal show optic-info ee port %d'
+IP = '10.52.3.3'
+portnumber = 10018
 
 def findeepromdump(output):
 	#output = output.splitlines()
@@ -22,13 +24,13 @@ def eepromprint(eeprom, out):
 		print(line)
 		out.write(line)
 
-good = open('gooddump.txt')
+good = open('davesgood.txt')
 debughaloutput = []
 goodeeproms = []
 portno= -1
 for line in good:
 	debughaloutput.append(line)
-	if line.startswith('(Engineering)'):
+	if 'debug hal show op' in line:
 		portno += 1
 		eeprom = findeepromdump(debughaloutput)
 		if len(eeprom) > 1:
@@ -42,7 +44,7 @@ good.close()
 
 logfile = open('eepromdumplog.txt', 'w', 1)
 
-DUT = D.EXOSDevice('10.52.2.222', 10018)
+DUT = D.EXOSDevice(IP, portnumber)
 nochanges = True
 attemptcounter = 1
 
@@ -57,7 +59,7 @@ while(nochanges):
 	print('Logged in.')
 	time.sleep(5) # let things settle out a little.
 
-	for port in range(1,12):
+	for port in range(1,6):
 		print('Checking eeprom of port %d' % port)
 		logfile.write(DUT.read().decode())
 		DUT.write(geteeprom % port)
@@ -71,6 +73,9 @@ while(nochanges):
 			eepromstotest.append(eeprom)
 
 	print('Number of eeproms found: %d' % len(eepromstotest))
+	if len(eepromstotest) != len(goodeeproms):
+		print('Not all eeproms found.  Quitting.')
+		nochanges = False
 
 	for good,test in zip(goodeeproms,eepromstotest):
 		for goodline,testline in zip(good,test):
@@ -85,7 +90,7 @@ while(nochanges):
 		print('Nothing found.  Rebooting.')
 		print('****************')
 		DUT.reset()
-		time.sleep(150)
+		time.sleep(170)
 		logfile.write(DUT.read().decode())
 
 logfile.close()
